54565ec1f90ee6fc510e30271f2e207f6faa4454
DavidMichaelson <dovydasbalciunas95@gmail.com>
2025-04-22 18:30:22 +0300

    done task

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+
+        double number =(double) toBeDivided / 5;
+        System.out.println(number);
+    }
+
+    public static void main(String[] args) {
+        NumberDivider divider = new NumberDivider();
+        divider.divide(219);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,16 @@
 package lang.print.gaps.task5;
 
+import java.sql.Time;
+
 public class TimeConvertor {
     public void convert(float minutes) {
+
+        int seconds = (int) (minutes * 60);
+        System.out.println(minutes + " minutes are " + seconds + " seconds");
+    }
+
+    public static void main(String[] args) {
+        TimeConvertor convertor = new TimeConvertor();
+        convertor.convert(-2.45f);
     }
 }
