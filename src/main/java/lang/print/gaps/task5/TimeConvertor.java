e14fcfb72e05b5747585fbbf07fddab9629f6887
DavidMichaelson <dovydasbalciunas95@gmail.com>
2025-04-22 18:35:33 +0300

    fixing for max score

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -6,7 +6,7 @@ public class TimeConvertor {
     public void convert(float minutes) {
 
         int seconds = (int) (minutes * 60);
-        System.out.println(minutes + " minutes are " + seconds + " seconds");
+        System.out.println(seconds);
     }
 
     public static void main(String[] args) {
